1439a80b01379181e03868b17dbab7ada5aa1807
Mohammad Jalili <mo.jalili73@gmail.com>
2022-12-06 15:13:05 +0330

    update to rest from socket

---
+++ src/main/java/org/mjbot/service/TradingServiceTest.java
@@ -0,0 +1,8 @@
+package org.mjbot.service;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Profile("dev")
+public class TradingServiceTest {}
